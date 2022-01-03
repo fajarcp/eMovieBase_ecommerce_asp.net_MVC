6c59ffce470e7d1e218751cfa357a8d1c517d675
fajarcp <fajarfaju82@gmail.com>
2022-01-03 20:01:13 +0530

    oktoday

--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,4 @@
+using eMovieBase.data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -23,6 +24,8 @@ namespace eMovieBase
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //DbContextConfigurations
+            services.AddDbContext<AppDbContext>();
             services.AddControllersWithViews();
         }
 
---
+++ data/AppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using eMovieBase.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eMovieBase.data
+{
+    public class AppDbContext : DbContext
+    {
+        protected AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Actor_Movie>().HasKey(am => new
+            {
+                am.ActorId,
+                am.MovieId
+
+            });
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
